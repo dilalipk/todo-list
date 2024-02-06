967eca0efd2ca54747f7613d6eed5a2fb9e43a35
Dilshad Ali <dilalipk@gmail.com>
2024-02-06 18:19:28 +0500

    Add models and add task functionality

--- TodoGuru/views.py
+++ TodoGuru/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 from django.shortcuts import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Home Page")
+    return render(request, "index.html")
--- taskApp/views.py
+++ taskApp/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponse
+from .models import Task
+from django.contrib.auth.models import User
+from .forms import TaskForm
+from django.contrib import messages as msg
+
 # Create your views here.
 
+
 def all_tasks(request):
-    return HttpResponse("This is a list of tasks")
+    if request.method == "POST":
+        forms = TaskForm(request.POST)
+        print(forms)
+        if forms.is_valid():
+            task = forms.save(commit=False)
+            task.author = request.user
+            task.save()
+
+        return redirect("tasks")
+    if request.method == "GET":
+
+        tasks = Task.objects.all()
+        context = {
+            'tasks': tasks
+        }
+
+        return render(request, "todolist.html" , context=context)
