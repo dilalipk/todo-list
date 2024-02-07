82138e71e5e8c7c0c48b830be17ddaad5aa02802
Dilshad Ali <dilalipk@gmail.com>
2024-02-07 09:02:20 +0500

    Update The edit and delete buttons functionality

--- taskApp/urls.py
+++ taskApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_tasks, name='tasks'),
+    path("", views.all_tasks, name="tasks"),
+    path("edit/<id>", views.edit, name="edit"),
+    path("delete/<id>", views.delete, name="delete"),
 ]
--- taskApp/views.py
+++ taskApp/views.py
@@ -21,8 +21,23 @@ def all_tasks(request):
     if request.method == "GET":
 
         tasks = Task.objects.all()
-        context = {
-            'tasks': tasks
-        }
+        context = {"tasks": tasks}
 
-        return render(request, "todolist.html" , context=context)
+        return render(request, "todolist.html", context=context)
+
+
+def edit(request, id):
+
+    if request.method == "POST":
+        forms = TaskForm(request.POST, instance=Task.objects.get(id=id))
+        if forms.is_valid():
+            task = forms.save(commit=False)
+            task.author = request.user
+            task.save()
+            return redirect("tasks")
+
+
+def delete(request, id):
+    task = Task.objects.get(pk=id)
+    task.delete()
+    return redirect("tasks")
